9235791f870fc4d8f8b6d4d29a6a95f461920335
Yong Kuk Kim <ykkim6@hotmail.com>
2025-09-20 19:59:42 -0400

    testing email (16)

--- src/app/api/sendEmail/route.tsx
+++ src/app/api/sendEmail/route.tsx
@@ -1,7 +1,7 @@
-import { Resend } from "resend";
-import EmailTemplate from "@/components/EmailTemplate/EmailTemplate";
+// import { Resend } from "resend";
+// import EmailTemplate from "@/components/EmailTemplate/EmailTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   console.log("sendEmail API hit!");
